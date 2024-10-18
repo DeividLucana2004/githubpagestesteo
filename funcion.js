a77bc1f75f0931a2dad4eee8afabab126abebf74
David Lucana <mrbigotescopilot34@gmail.com>
2024-10-18 16:52:26 -0400

    Cambios con javascript

---
+++ funcion.js
@@ -0,0 +1,12 @@
+function toggleRespuesta(index) {
+    const respuestas = document.querySelectorAll('.respuesta');
+    const flechas = document.querySelectorAll('.flecha');
+    
+    if (respuestas[index].style.display === "none") {
+        respuestas[index].style.display = "block";
+        flechas[index].innerHTML = "&#9650;";
+    } else {
+        respuestas[index].style.display = "none";
+        flechas[index].innerHTML = "&#9660;";
+    }
+}
