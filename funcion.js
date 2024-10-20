b76fc4353ed3d6daa40711344a6eb7cfb551a82f
David Lucana <mrbigotescopilot34@gmail.com>
2024-10-19 20:35:36 -0400

    Cambios modificados de imagenes

--- funcion.js
+++ funcion.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const juntarlo = document.querySelector('.juntarlo');
+
+    // Asegúrate de que el contenedor sea visible
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                juntarlo.style.display = 'flex'; // Muestra el contenedor
+            } else {
+                juntarlo.style.display = 'none'; // Oculta el contenedor
+            }
+        });
+    });
+
+    observer.observe(juntarlo);
+});
+
+
 function toggleRespuesta(index) {
     const respuestas = document.querySelectorAll('.respuesta');
     const flechas = document.querySelectorAll('.flecha');
