6e01354b3ff5324beba6e49a9188bef6d11ad40f
David Lucana <mrbigotescopilot34@gmail.com>
2024-10-22 00:33:06 -0400

    Cursor predeterminado

--- funcion.js
+++ funcion.js
@@ -1,3 +1,35 @@
+// Seleccionamos los elementos de texto
+const frontendText = document.querySelector('.frontend');
+const nombreText = document.querySelector('.nombre');
+const carets = document.querySelectorAll('.caret');
+
+// Función para cambiar las clases
+function cambiarColor() {
+    // Agregamos la clase 'selected' para cambiar de color
+    frontendText.classList.add('selected');
+    nombreText.classList.add('selected');
+
+    // Oculta el cursor temporalmente
+    carets.forEach(caret => caret.style.display = 'none');
+
+    // Regresa el cursor después de 1 segundo
+    setTimeout(() => {
+        carets.forEach(caret => caret.style.display = 'inline');
+    }, 100);
+
+    // Elimina la clase 'selected' después de 3 segundos
+    setTimeout(() => {
+        frontendText.classList.remove('selected');
+        nombreText.classList.remove('selected');
+    }, 3000); // Cambia esto a 4000 para incluir el tiempo de parpadeo
+}
+
+// Ejecutamos la función de cambiar color cada 6 segundos
+setInterval(cambiarColor, 6000);
+
+// Llamamos a la función una vez al inicio
+cambiarColor();
+
 
 
 function toggleRespuesta(index) {
