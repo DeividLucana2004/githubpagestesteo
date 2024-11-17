d16a7a4cdbb08e84a3bcb74350aad2a0b2b239ff
David Lucana <mrbigotescopilot34@gmail.com>
2024-11-17 01:29:15 -0400

    er

--- funcion.js
+++ funcion.js
@@ -4,26 +4,7 @@ function toggleSubmenu(){
     submenu.style.display = submenu.style.display === "block" ? "none" : "block";
 }
 //Login 
-//Login
-const wrapper = document.querySelector('.wrapper');
-const registerLink = document.querySelector('.register-link');
-const loginLink = document.querySelector('.login-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close')
 
-registerLink.onclick = () => {
-    wrapper.classList.add('active');
-};
-loginLink.onclick = () => {
-    wrapper.classList.remove('active');
-};
-btnPopup.onclick = () => {
-    wrapper.classList.add('active-popup');
-};
-iconClose.onclick = () => {
-    wrapper.classList.remove('active-popup');
-    wrapper.classList.remove('active')
-};
 
 
 
